2182f8e55abcff4d296c35b8359a1e5b8112b963
NormalPlayerJSH <jsh010904@gmail.com>
2023-11-10 13:41:10 +0900

    오류 시 처리

--- src/app/api/run/route.ts
+++ src/app/api/run/route.ts
@@ -16,12 +16,15 @@ export interface PythonReturnI {
 }
 
 export async function POST(request: Request) {
-  const { setting, code: innerCode } = await request.json() as RunRequestDataI;
-  const code = makePythonCode(setting, innerCode, 'testt.png')
-  const pythonProcess = await spawnSync('python3', ['-c', code])
-  const result = pythonProcess.stdout?.toString()?.trim();
-  const error = pythonProcess.stderr?.toString()?.trim();
-  const data = JSON.parse(result)
-
-  return NextResponse.json(data)
+    const { setting, code: innerCode } = await request.json() as RunRequestDataI;
+    const code = makePythonCode(setting, innerCode, 'testt.png')
+    const pythonProcess = await spawnSync('python3', ['-c', code])
+    const result = pythonProcess.stdout?.toString()?.trim();
+    const error = pythonProcess.stderr?.toString()?.trim();
+    try {
+      const data = JSON.parse(result)
+      return NextResponse.json(data)
+    } catch {
+      return NextResponse.json({ image: '', print: `오류가 발생했습니다\n\n출력값\n${result}${error}`})
+    }
 }
